531ca45faf10befa01cd64098dda425e21cd9f87
ytding <2441777238@qq.com>
2023-11-29 22:37:21 +0800

    查询黑名单

---
+++ 后端部分/查询黑名单/mapper/BlackEntityMapper.java
@@ -0,0 +1,22 @@
+package com.example.blacklist.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * @Author: ytding
+ * @Date: 2023/11/29 21:27
+ * @Description:
+ * @Version: 1.0
+ */
+@Mapper
+public interface BlackEntityMapper {
+
+    /**
+     * 查询黑名单
+     * @param username
+     * @return
+     */
+    List<String> selectByUsername(String username);
+}
