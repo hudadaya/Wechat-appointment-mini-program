4a3b217a18df41b84836cbc1d96a9c7101d6d89a
wuyuqinglll <126392515+wuyuqinglll@users.noreply.github.com>
2024-01-05 16:08:39 +0800

    删除不必要的代码，对整个后端代码文件夹说明

--- 后端部分/后端代码/整合的代码/demo/src/main/java/com/example/demo/service/UserLoginService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.example.demo.entity.UsersEntity;
-
-public interface UserLoginService extends IService<UsersEntity> {
-    UsersEntity getselectByUserName(String username,String pwd);
-    Boolean isBlack(String username);
-}
